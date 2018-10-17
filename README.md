f4ef397a64a98010eac60cc16763567196c4b155
abd0m0hamed <abdo.tasks@gmail.com>
2018-10-17 23:51:18 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+## Description
+
+<p>Simple responsive HTML & JS script to publish Web Books.</p>
+
+## Installation
+
+* Place book pages in jpg format inside the folder <b> pages</b>.
+* All images must be named in sequence from 1 to any number (Number of Book pages), Ex: 1.jpg, 2.jpg, 3.jpg, etc..
+* Open the file <b>index.html</b> and modify the <b>numberOfPages</b> variable to your book number of pages.
+
+
+
---
+++ js/scripts.js
@@ -0,0 +1,103 @@
+/*
+* Author: Abdelrahman Mohamed
+* Contact: < Abdo.Tasks@Gmail.Com , https://Github.com/abd0m0hamed >
+* Project: WebBook 1.0
+* Description: Simple responsive HTML & JS script to publish Web Books.
+* License: Science not for Monopoly.
+*/
+function webBook(){
+
+    var self = this;
+    this.actionToDo = null;
+    this.numberOfPages = 10;
+    this.pageToGo = 0;
+    
+    this.init = function(numberOfPages){
+        
+        if(numberOfPages){
+            self.numberOfPages = numberOfPages;
+        }
+        
+        self.setArrowsPosition();
+        
+        $(".arrow-next").click(function(){
+            self.setActionToDo("next");
+            self.nextPage();
+        });
+        
+        $(".arrow-prev").click(function(){
+            self.setActionToDo("prev");
+            self.prevPage();
+        });
+    
+    }
+    
+    this.getCurrentPage = function(){
+        return parseInt($("#pageImg").attr("pageNumber"));
+    }
+    
+    this.setCurrentPage = function(pageNumber){
+        $("#pageImg").attr("pageNumber",pageNumber);
+    }
+    
+    this.setArrowsPosition = function(){
+        var arrowPositionY = parseInt($("#pageImg").height() / 2);
+        $(".arrow-next, .arrow-prev").css("top",arrowPositionY+"px");
+    }
+    
+    this.setActionToDo = function(action){
+        self.actionToDo = action;
+    }
+    
+    this.getActionToDo = function(){
+        return self.actionToDo;
+    }
+    
+    this.setLoading = function(){
+        $("#pageImg").attr("src","images/loader.gif");
+        $(".arrow-next, .arrow-prev").hide();
+    }
+    
+    this.clearLoading = function(){
+        $(".arrow-next, .arrow-prev").show();
+    }
+    
+    this.loadPage = function(){
+        
+        self.setCurrentPage(self.pageToGo);
+        
+        var ImageLoader = new Image();
+        ImageLoader.src = "pages/" + self.pageToGo + ".jpg";
+        ImageLoader.onload = function(){
+            $("#pageImg").attr("src",ImageLoader.src);
+            self.clearLoading();
+        }
+        
+    }
+    
+    this.nextPage = function(){
+        
+        if(self.getCurrentPage() === self.numberOfPages && self.getActionToDo()  === 'next'){
+            return false;
+        }
+        
+        self.pageToGo = self.getCurrentPage() + 1;
+        self.setLoading();
+        self.loadPage();
+        
+    }
+    
+    this.prevPage = function(){
+    
+        if(self.getCurrentPage() === 1 && self.getActionToDo() === 'prev'){
+            return false;
+        }
+        
+        self.pageToGo = self.getCurrentPage() - 1;
+        self.setLoading();
+        self.loadPage();
+    }
+    
+    return self;
+};
+
